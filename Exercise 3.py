a1b88f907db44420fdb1cc16b4f776f36a2c0887
Kunzal Khawaja Fahim <kunzalfahim04@gmail.com>
2024-11-11 18:51:28 +0400

    Create Exercise 3.py

---
+++ Exercise 3.py
@@ -0,0 +1,9 @@
+#Exercise 3: Biography 
+print("Fill in your details:")
+N = input("Enter your full name:")
+H = input("Enter your hometown:")
+A = int[input("Enter your age:")]
+
+print(N)
+print(H)
+print(A)

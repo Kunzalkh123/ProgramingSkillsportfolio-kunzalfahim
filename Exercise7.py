f1af86691a8c09d81b66f083c9811c35947f5261
Kunzal Khawaja Fahim <kunzalfahim04@gmail.com>
2024-11-19 18:50:50 +0400

    Update Exercise7.py

--- Exercise7.py
+++ Exercise7.py
@@ -1,3 +1,15 @@
 #Exercise 7: Some Counting
 for i in range(0, 50):
     print(i)
+
+for i in range(50, -1, -1):
+    print(i)
+
+for i in range(30, 50):
+  print(i)
+
+for i in range(50, 10, -2):
+    print(i)
+
+for i in range(100, 201, 5):
+    print(i)

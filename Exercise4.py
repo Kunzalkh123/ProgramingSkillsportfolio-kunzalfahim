5cfab550cd9dc12b6e180c6d00429c1dabc73030
Kunzal Khawaja Fahim <kunzalfahim04@gmail.com>
2024-11-19 18:29:54 +0400

    error
    modifications required

---
+++ Exercise4.py
@@ -0,0 +1,7 @@
+#Exercise 4: Primitive Quiz 
+Capital = input("What is the capital of France? ")
+if Capital().lower() == "Paris".lower():
+    print("Well done! The capital of France is Paris.")
+else:
+    print("Incorrect! The correct answer is Paris.")
+    
